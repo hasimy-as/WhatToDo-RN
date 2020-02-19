3b67f56d8b1596ad32e04f8e06f60eee615fa821
hasimy-as <ahmadhasyimi674@gmail.com>
2020-02-19 14:20:38 +0700

    finish

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import ToDo from './src/ToDo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <ToDo />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
